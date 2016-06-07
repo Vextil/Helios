d5dd6a59aefa48f8ae0aa8e695e36083f7ff64e0
Joaquin Cuitiño <vextil@gmail.com>
2016-06-07 20:32:05 -0300

    Continue "Settings" area

---
+++ app/src/main/kotlin/io/vextil/launcher/fragments/settings/AppsSettingsFragment.kt
@@ -0,0 +1,18 @@
+package io.vextil.launcher.fragments.settings
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.vextil.launcher.R
+
+class AppsSettingsFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.fragment_settings_apps, container, false)
+
+        return view
+    }
+
+}
--- app/src/main/kotlin/io/vextil/launcher/fragments/settings/WebAppsSettingsFragment.kt
+++ app/src/main/kotlin/io/vextil/launcher/fragments/settings/WebAppsSettingsFragment.kt
@@ -2,11 +2,12 @@ package io.vextil.launcher.fragments.settings
 
 import android.app.Activity
 import android.os.Bundle
+import io.vextil.launcher.*
 
 class WebAppsSettingsFragment: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        setContentView(R.layout.fragment_settings_webapps)
     }
 
 }
