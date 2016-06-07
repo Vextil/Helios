08decbe59cbfc250200aedc78d287afc2d3ad2cd
Joaquin Cuitiño <vextil@gmail.com>
2016-06-07 20:02:30 -0300

    Add settings theme and WebApp settings fragment

---
+++ app/src/main/kotlin/io/vextil/launcher/fragments/settings/WebAppsSettingsFragment.kt
@@ -0,0 +1,12 @@
+package io.vextil.launcher.fragments.settings
+
+import android.app.Activity
+import android.os.Bundle
+
+class WebAppsSettingsFragment: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+    }
+
+}
