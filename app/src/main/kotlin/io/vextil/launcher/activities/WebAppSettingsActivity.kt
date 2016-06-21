ec0cf97264b8612cf8dc72ce31b6d3f7819b09e8
Joaquin Cuitiño <vextil@gmail.com>
2016-06-21 20:19:36 -0300

    Update WebAppSettingsActivity

--- app/src/main/kotlin/io/vextil/launcher/activities/WebAppSettingsActivity.kt
+++ app/src/main/kotlin/io/vextil/launcher/activities/WebAppSettingsActivity.kt
@@ -3,12 +3,14 @@ package io.vextil.launcher.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.vextil.launcher.R
+import kotlinx.android.synthetic.main.activity_web_app_settings.*
 
 class WebAppSettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_app_settings)
-        
+        setSupportActionBar(toolbar)
+
     }
 }
