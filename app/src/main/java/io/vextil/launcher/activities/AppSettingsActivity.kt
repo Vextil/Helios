155f6a7722b31af968203ba1060623214c41b37b
Joaquin Cuitiño <vextil@gmail.com>
2016-06-26 11:20:44 -0300

    Restructure folders

---
+++ app/src/main/java/io/vextil/launcher/activities/AppSettingsActivity.kt
@@ -0,0 +1,13 @@
+package io.vextil.launcher.activities
+
+import android.app.Activity
+import android.os.Bundle
+import io.vextil.launcher.R
+
+class AppSettingsActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_settings)
+    }
+}
